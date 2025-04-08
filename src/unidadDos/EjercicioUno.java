bd41742d7c390b6f60324ea1172db26827929d5e
Pesgadaffi <programas943@gmail.com>
2025-04-08 07:44:12 -0500

    Ejercicios Unidad Dos

---
+++ src/unidadDos/EjercicioUno.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class EjercicioUno {
+    public static void main(String[] args) {
+        int[] vectorNumeros = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < vectorNumeros.length; i++) {
+            vectorNumeros[i] = rand.nextInt(10) + 1; 
+        }
+        System.out.println("Número | Cuadrado | Cubo");
+        System.out.println("--------------------------");
+        for (int i = 0; i < vectorNumeros.length; i++) {
+            int numero = vectorNumeros[i];
+            
+            System.out.println(numero + " | " + (numero * numero) + " | " + (numero * numero * numero));
+        }
+    }
+}
